6c8fa0247987ceb63101b9e757cc15e84f3f0b76
CSL <christasaraluke19@gmail.com>
2024-07-04 17:44:42 -0700

    Added green rectangles to objects that enters the frame.

--- main.py
+++ main.py
@@ -3,10 +3,27 @@ import time
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
+original_frame = None
 
 while True:
     check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gau_gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if original_frame is None:
+        original_frame = gau_gray_frame
+
+    del_frame = cv2.absdiff(original_frame, gau_gray_frame)
+    thresh_frame = cv2.threshold(del_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=3)
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        rect_frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
     cv2.imshow('My Video', frame)
+
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
